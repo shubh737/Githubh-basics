1c86d57d9a9e7e1d71c5f78e4d3bf5fc5858593c
shubh737 <72033363+shubh737@users.noreply.github.com>
2021-02-15 21:18:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var score;
 function preload(){
   
   
-  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
+  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
   
   bananaImage = loadImage("banana.png");
   obstacleImage = loadImage("obstacle.png");
